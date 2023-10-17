beb85d60f44bc2ab37e5889f10c9ecbf8331703f
Pugud Pambudi <noerbayu73@gmail.com>
2023-10-18 01:12:46 +0700

    update github pages setting

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/vue-app.github.io/' : '/'
+  // publicPath: process.env.NODE_ENV === 'production' ? '/vue-app.github.io/' : '/'
+  // base: process.env.BASE_URL === 'production' ? '/vue-app.github.io/' : '/'
+  baseUrl: process.env.VUE_APP_MODE === 'production' ? '/vue-app.github.io/' : '/',
 })
