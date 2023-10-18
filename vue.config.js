3afcafb6a0823f7febea8dbb635cc2fc24304049
Pugud Pambudi <noerbayu73@gmail.com>
2023-10-18 10:14:44 +0700

    deploy netliy files

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/vue-app.github.io/'
+  // publicPath: '/vue-app.github.io/'
+  pwa: {
+    workboxOptions: {
+      exclude: [/_redirects/]
+    }
+  }
 })
